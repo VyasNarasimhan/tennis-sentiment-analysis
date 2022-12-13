bcd5ddcf9b6ad7ca63ed73a13bf3d48218b7bd97
Abhi6 <abhinav.mara@gmail.com>
2022-12-13 11:58:13 -0500

    Modified get_rating and get_tweets so that get_rating takes in a list of tweets and get_tweets return a 2d list of tweets that have been sectioned off

--- server/twitter.py
+++ server/twitter.py
@@ -14,16 +14,12 @@ class Tweet:
         self.BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
         self.url = 'https://api.twitter.com/2/tweets/search/recent'
 
-    def get_rating_multiprocessing(self, tweet_list):
-        start = time.perf_counter()
+    def get_rating(self, tweet_list):
         sentiment_pipeline = pipeline('sentiment-analysis')
-        sentiments = [sentiment_pipeline(clean(tweet))[0] for tweet in tweet_list])
+        sentiments = ([sentiment_pipeline(clean(tweet))[0] for tweet in tweet_list])
         rating = sum([sent['score'] * (-1 if sent['label'] == 'NEGATIVE' else 1) for sent in sentiments]) / len(sentiments)
-        finish = time.perf_counter()
-        print(f'Execution time (multiprocessing): {finish-start}')
         return rating / 2 + 5
         
-
     def bearer_oauth(self, r):
         r.headers["Authorization"] = f"Bearer {self.BEARER_TOKEN}"
         r.headers["User-Agent"] = "v2RecentSearchPytho"
@@ -39,11 +35,33 @@ class Tweet:
                 )
             )
         return response.json()
+    
+    def break_up_tweets(self, section_length, all_tweets):
+        # create a list that contains lists of tweets
+        tweet_list_2d = []
+        
+        # bounds to section of 1d list of tweets
+        lower_bound = 0
+        upper_bound = section_length
+        
+        # section the tweets and append each to tweet_list_2d
+        while (upper_bound <= len(all_tweets)):
+            tweet_list_2d.append(all_tweets[lower_bound:upper_bound])
+            lower_bound += section_length
+            upper_bound += section_length
+
+        return tweet_list_2d
 
     def get_tweets(self, player):
         data = json.loads(json.dumps(tweet.connect_to_endpoint({'query': player + ' lang:en', 'tweet.fields': 'lang', 'max_results': 100})))
-        return [i['text'] for i in data['data']]
+        raw_tweets = [i['text'] for i in data['data']]
+        tweet_list_2d = self.break_up_tweets(10, raw_tweets)
+        # return [i['text'] for i in data['data']]
+        return tweet_list_2d
 
 if __name__ == '__main__':
     tweet = Tweet()
-    print(tweet.get_rating('novak djokovic'))
+    sectioned_tweets = tweet.get_tweets('novak djokovic')
+    print(sectioned_tweets)
+    for inner_tweets in sectioned_tweets:
+        print(tweet.get_rating(inner_tweets))
